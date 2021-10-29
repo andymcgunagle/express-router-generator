ab77c4a2c617f6cb9eda83cdece53870d851bdb9
andymcgunagle <amcgunagle@gmail.com>
2021-10-29 15:05:30 -0700

    Fixed missing test description

--- createRouterFunctions/tests/routeTypeExtractor.test.js
+++ createRouterFunctions/tests/routeTypeExtractor.test.js
@@ -1,9 +1,9 @@
 import routeTypeExtractor from '../routeTypeExtractor.js';
 
-test('extracts the ', () => {
+test('extracts the type of route', () => {
   expect(routeTypeExtractor('postRouteName')).toBe('post');
 });
 
-test('extracts the ', () => {
+test('extracts the type of route', () => {
   expect(routeTypeExtractor('additionalGetRouteName')).toBe('get');
 });
