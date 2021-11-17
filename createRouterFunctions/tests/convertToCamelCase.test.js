8f5442b802018bd0b26429466d683dc395aa6d82
andymcgunagle <amcgunagle@gmail.com>
2021-11-17 13:22:14 -0800

    Moved tests dir and added it to .gitignore file.

--- createRouterFunctions/tests/convertToCamelCase.test.js
+++
@@ -1,6 +0,0 @@
-import convertToCamelCase from '../convertToCamelCase.js';
-
-test('converts to camel case', () => {
-  expect(convertToCamelCase('my-router')).toBe('myRouter');
-  expect(convertToCamelCase('my-amazing-post-route')).toBe('myAmazingPostRoute');
-});
--- createRouterFunctions/tests/routeTypeExtractor.test.js
+++
@@ -1,6 +0,0 @@
-import routeTypeExtractor from '../routeTypeExtractor.js';
-
-test('extracts the type of route', () => {
-  expect(routeTypeExtractor('postRouteName')).toBe('post');
-  expect(routeTypeExtractor('additionalGetRouteName')).toBe('get');
-});
